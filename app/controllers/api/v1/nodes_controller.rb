7f50f582f417b17363be3e05434aaa39415077e9
David Eko Wibowo <dvdekow@gmail.com>
2014-08-03 18:07:08 +0700

    update create and show api

--- app/controllers/api/v1/nodes_controller.rb
+++ app/controllers/api/v1/nodes_controller.rb
@@ -34,34 +34,55 @@ class Api::V1::NodesController < Api::V1::BaseController
     	type = params[:type]
     end
 
-    nodeBuyer = @neo.create_node("userid" => userid)
-    labeledBuyer = @neo.add_label(nodeBuyer, "Buyer")
+    queryBuyer = @neo.execute_query("match (n) where n.userid = '#{userid}' return n")
 
-    nodeItem = @neo.create_node("itemid" => itemid)
-    labeledItem = @neo.add_label(nodeItem, "Item")
+    if queryBuyer["data"].empty?
+      nodeBuyer = @neo.create_node("userid" => userid)
+      labeledBuyer = @neo.add_label(nodeBuyer, "Buyer")
+    else
+      nodeBuyer = queryBuyer["data"]
+    end
+
+    queryItem = @neo.execute_query("match (n) where n.itemid = '#{itemid}' return n")
+
+    if queryItem["data"].empty?
+      nodeItem = @neo.create_node("itemid" => itemid)
+      labeledItem = @neo.add_label(nodeItem, "Item")
+    else
+      nodeItem = queryItem["data"]
+    end
 
     # create relationship
-    rel = @neo.create_relationship("look", nodeBuyer, nodeItem)
+    rel = @neo.create_relationship(type, nodeBuyer, nodeItem)
 
     # return buyer node id, for updating when userid acquired
 
     # labelnode = @neo.add_label(unlabelnode, "Buyer")
     # newnode = @neo.create_node("userid" => "davideko")
     unless rel.nil?
-      render json: {:node => rel, :message => 'Look relation has been created'}
+      render json: {:node => rel, :message => 'look relation has been created'}
   	else
       render json: {:message => 'Node has not been created'}
     end
   end
 
   def show
-    @node = Node.find(params[:id])
+  	# this method will return node with id as userid or itemid properties
+    id = params[:id]
     # initiate neography
-  	@neo = Neography::Rest.new
-  	testnode = @neo.execute_query("match (n) where n.userid = 'davideko' return n")
-  	testnode2 = @neo.execute_query("match (n) where n.itemid = 'tv234' return n")
-  	rela = @neo.create_relationship("look", testnode["data"], testnode2["data"])
-   	render json: {:node => testnode["data"], :message => 'Node found'}
+    @neo = Neography::Rest.new
+    uquerynode = @neo.execute_query("match (n) where n.userid = '#{id}' return n")
+    iquerynode = @neo.execute_query("match (n) where n.itemid = '#{id}' return n")
+  	
+  	# nodeBuyer = @neo.create_node("userid" => "wihihi")
+    # labeledBuyer = @neo.add_label(nodeBuyer, "Buyer")
+    if uquerynode["data"].empty? && iquerynode["data"].empty?
+   	   render json: {:message => 'Node not found'}
+   	elsif iquerynode["data"].empty?
+   	   render json: {:node => uquerynode["data"],:message => 'Node user found'}
+   	else 
+   	   render json: {:node => iquerynode["data"], :message => 'Node item found'}
+   	end
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Finalproject::Application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :nodes
-      resources :relation
     end
   end
   # The priority is based upon order of creation:
